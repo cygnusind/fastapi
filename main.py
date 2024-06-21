be539f1c3a886ae08ed2f1465a342481cafe900b
HARENCYGNUSIND <150027675+HARENCYGNUSIND@users.noreply.github.com>
2024-06-21 18:51:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ headers = {
 async def mutli_pro():
     async with httpx.AsyncClient() as client:
         response = await client.post("https://api.bakuun.com/ratedockAPI/RDK220/getproperty" ,json=MainModal,headers=headers)
-        return response.json()
+        return MainModal
    
