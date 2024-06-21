850c406285c0431b22d26627fbc858bea8b8baaf
HARENCYGNUSIND <150027675+HARENCYGNUSIND@users.noreply.github.com>
2024-06-21 18:46:39 +0530

    added mps function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-import asyncio
+# import asyncio
 import httpx
-
+from typing import Union
+from enum import Enum
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+from typing_extensions import Annotated
 app = FastAPI()
 
 @app.get("/")
@@ -17,9 +21,9 @@ async def test():
     async with httpx.AsyncClient() as client:
         response = await client.get("https://httpbin.org/get")
     return response.json()
-
+#GET PROPERTY
 @app.post("/Getproperty/")
-async def test1(proid:str):
+async def GetProperty(proid:str):
     async with httpx.AsyncClient() as client:
         payload = {
     "authentication": {
@@ -39,4 +43,31 @@ async def test1(proid:str):
 
 
     return response.json()
+
+
+
+
+#MPS
+class mutliSeacrhIds(str,Enum):
+    propertyIds: str
+
+class ratesByOccupancy1(BaseModel):
+   occupancies:list
+
+class MainModal(BaseModel):
+    username:str
+    password:str
+    partnerId:str
+    multiSearchByIds:mutliSeacrhIds
+    ratesByOccupancy:ratesByOccupancy1
+
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+}
+@app.post("/mps/")
+async def mutli_pro():
+    async with httpx.AsyncClient() as client:
+        response = await client.post("https://api.bakuun.com/ratedockAPI/RDK220/getproperty" ,json=MainModal,headers=headers)
+        return response.json()
    
