72e16c97e6e8ed82b29cdf68c72d2758e7e0e148
HARENCYGNUSIND <150027675+HARENCYGNUSIND@users.noreply.github.com>
2024-06-24 14:44:07 +0530

    update main.py_getprop

--- main.py
+++ main.py
@@ -78,4 +78,4 @@ async def root():
     async with httpx.AsyncClient() as client:
         body = await request.body()
         response = await client.get("https://api.bakuun.com/ratedockAPI/RDK220/getproperty",content=body, headers=request.headers)
-    return response.json()
+    return body
