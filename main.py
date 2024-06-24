c354a7c325b23cfd82e43e5b5bbf3584f55435c8
HARENCYGNUSIND <150027675+HARENCYGNUSIND@users.noreply.github.com>
2024-06-24 16:06:02 +0530

    removed functions

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 # import asyncio
 import httpx
-from typing import Union
-from enum import Enum
-from pydantic import BaseModel, Field
-from pydantic.config import ConfigDict
-from typing_extensions import Annotated
+
 app = FastAPI()
 
 @app.get("/")
@@ -21,56 +17,7 @@ async def test():
     async with httpx.AsyncClient() as client:
         response = await client.get("https://httpbin.org/get")
     return response.json()
-#GET PROPERTY
-@app.post("/Getproperty/")
-async def GetProperty(proid:str):
-    async with httpx.AsyncClient() as client:
-        payload = {
-    "authentication": {
-        "username": "cygnus",
-        "password": "KdFoGuC_",
-        "propertyId": proid,
-        "partnerId": "RDK220"
-    },
-    "action": "GetPropertyInfo"
-}
-        headers = {
-    "accept": "application/json",
-    "content-type": "application/json"
-}
-        
-        response = await client.post("https://api.bakuun.com/ratedockAPI/RDK220/getproperty" ,json=payload,headers=headers)
 
-
-    return response.json()
-
-
-
-
-#MPS
-class mutliSeacrhIds1(BaseModel):
-    propertyIds: list[str, None] = None
-
-class ratesByOccupancy1(BaseModel):
-   occupancies:list
-
-class MainModal(BaseModel):
-    model_config = ConfigDict(title='Main')
-    username:str
-    password:str
-    partnerId:str
-    multiSearchByIds:mutliSeacrhIds1
-    #ratesByOccupancy:ratesByOccupancy1
-
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json"
-}
-@app.post("/mps")
-async def mutli_pro(model:MainModal):
-    async with httpx.AsyncClient() as client:
-        #response = await client.post("https://api.bakuun.com/ratedockAPI/RDK220/getproperty" ,json=MainModal,headers=headers)
-        return model
 
 
 @app.post("/getprop")
