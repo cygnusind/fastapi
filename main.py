8cd78290342ce8bdcc028480d3d36dfa16744ebf
cygnusind <niyas.yousuf@cygnusind.in>
2024-06-24 12:01:23 +0000

    request

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def test():
 @app.post("/getprop")
 async def root1(request: Request):
     async with httpx.AsyncClient() as client:
-        body = await request.body()
+        body = await request
         response = await client.post("https://api.bakuun.com/ratedockAPI/RDK220/getproperty",json=body, headers=request.headers)
         #,content=body, headers=request.headers
     return body
