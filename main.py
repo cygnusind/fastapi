952d48c6bc9e245e956e6948a613d0bb20ee049c
Haren <150027675+HARENCYGNUSIND@users.noreply.github.com>
2024-12-20 17:05:43 +0530

    added print in book confirmation

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def generate_pdf_from_html(html_content):
 
 @app.post("/booking-confirmation")
 async def booking_confirmation(data: BookingData):
+    print(data)
     # Open and read the HTML file
     with open("voucher.html", "r") as file:
         html_content = file.read()
